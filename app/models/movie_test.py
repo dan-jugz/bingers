143995c775a83431c52ed1bacb5ca6c0bf905ff3
daniel njuguna <njugunadaniel364@gmail.com>
2019-07-23 17:08:43 +0300

    define a movie class with the parameters we want in the movie objects

--- app/models/movie_test.py
+++ app/models/movie_test.py
@@ -10,7 +10,7 @@ class MovieTest(unittest.TestCase):
         '''
         setup method that will run before every test
         '''
-                self.new_movie = Movie(1234,'Python Must Be Crazy','A thrilling new Python Series','https://image.tmdb.org/t/p/w500/khsjha27hbs',8.5,129993)
+        self.new_movie = Movie(1234,'Python Must Be Crazy','A thrilling new Python Series','https://image.tmdb.org/t/p/w500/khsjha27hbs',8.5,129993)
 
     def test_instance(self):
         self.assertTrue(isinstance(self.new_movie,Movie))
