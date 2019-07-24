7853aeb35ee59ca36abc2133380bf576eee95d4c
daniel njuguna <njugunadaniel364@gmail.com>
2019-07-24 13:00:49 +0300

    add the now playing and upcoming list

--- app/views.py
+++ app/views.py
@@ -8,13 +8,15 @@ def index():
     '''
     view root page function that returns the index page and its data
     '''
-    message = 'world'
-    return render_template('index.html',message=message)
+
     # Getting popular movie
     popular_movies = get_movies('popular')
-    print(popular_movies)
+    upcoming_movie = get_movies('upcoming')
+    now_showing_movie = get_movies('now_playing')
+    
     title = 'Home - Welcome to The best Movie Review Website Online'
-    return render_template('index.html', title = title,popular = popular_movies)
+    message = ' Idlers'
+    return render_template('index.html', title = title, popular = popular_movies,upcoming = upcoming_movie,now_playing = now_showing_movie, message=message)
 
 @app.route('/movie/<int:movie_id>')
 def movie(movie_id):
