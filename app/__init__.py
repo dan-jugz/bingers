2fb9689570a927c2ee96a98f8f8cd9d8c53d0a9b
daniel njuguna <njugunadaniel364@gmail.com>
2019-07-31 12:00:39 +0300

    import sqlalchemy to the app

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,22 @@
 from flask import Flask
 from config import DevConfig
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
 
+bootstrap = Bootstrap()
+db = SQLAlchemy()
 
 def create_app(config_name):
     # Initializing application
-    app = Flask(__name__,instance_relative_config = True)
-
+    app = Flask(__name__)
     # Setting up configuration
     app.config.from_object(DevConfig)
     app.config.from_pyfile('config.py')
 
-    #Initialising Flask Extension
-    bootstrap = Bootstrap(app)
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+    
     # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
